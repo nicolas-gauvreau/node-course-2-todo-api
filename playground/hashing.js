3a0c505716bac0d65532e875a5a55b00cf473fd7
Nicolas Gauvreau <GauvreNi@int.videotron.com>
2017-02-25 16:50:17 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abc');
-console.log({token, decoded});
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashed = '$2a$10$6TA41/Dj/9HSmmvgVxwJIOyGd4xHVWorcmQstT9qL8rJlCecBdG3m';
+bcrypt.compare(password, hashed, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// var decoded = jwt.verify(token, '123abc');
+// console.log({token, decoded});
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
