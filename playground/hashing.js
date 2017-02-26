d4292daab6ddb2c323c425048371ae39d4dc484f
Nicolas Gauvreau <GauvreNi@int.videotron.com>
2017-02-25 21:26:03 -0500

    Update config to use json file

--- playground/hashing.js
+++ playground/hashing.js
@@ -10,25 +10,25 @@ var password = '123abc!';
 //   });
 // });
 
-var hashed = '$2a$10$6TA41/Dj/9HSmmvgVxwJIOyGd4xHVWorcmQstT9qL8rJlCecBdG3m';
-bcrypt.compare(password, hashed, (err, res) => {
-  console.log(res);
-});
-
-// var data = {
-//   id: 10
-// };
-
-// var token = jwt.sign(data, '123abc');
-// var decoded = jwt.verify(token, '123abc');
-// console.log({token, decoded});
-
-// var message = "I am user number 3";
-// var hash = SHA256(message).toString();
-//
-// console.log({
-//   message, hash
+// var hashed = '$2a$10$6TA41/Dj/9HSmmvgVxwJIOyGd4xHVWorcmQstT9qL8rJlCecBdG3m';
+// bcrypt.compare(password, hashed, (err, res) => {
+//   console.log(res);
 // });
+
+var data = {
+  id: 10
+};
+
+var token = jwt.sign(data, process.env.JWT_SECRET);
+var decoded = jwt.verify(token, process.env.JWT_SECRET);
+console.log({token, decoded});
+
+var message = "I am user number 3";
+var hash = SHA256(message).toString();
+
+console.log({
+  message, hash
+});
 //
 // var data = {
 //   id: 4
@@ -41,7 +41,7 @@ bcrypt.compare(password, hashed, (err, res) => {
 // token.data.id = 5;
 // token.hash = SHA256(JSON.stringify(token.data)).toString();
 //
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// var resultHash = SHA256(JSON.stringify(token.data) + proces.env.JWT_SECRET).toString();
 // if (resultHash === token.hash) {
 //   console.log('Data was not changed.');
 // } else {
--- server/models/user.js
+++ server/models/user.js
@@ -41,7 +41,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
   user.tokens.push({ access, token });
   return user.save().then(() => {
     return token;
@@ -61,7 +61,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
@@ -91,7 +91,7 @@ UserSchema.statics.findByCredentials = function(email, password) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
