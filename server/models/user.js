da3fd26b863885d2df6b35825fe3c1da4fa94361
Nicolas Gauvreau <GauvreNi@int.videotron.com>
2017-02-25 19:21:51 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -47,6 +47,15 @@ UserSchema.methods.generateAuthToken = function() {
     return token;
   });
 };
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+    $pull: {
+        tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
